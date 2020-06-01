0603462fe4da0b31bded39d9ca87d6957c6869b5
William Pereira <williamfunchal@outlook.com>
2020-06-01 12:54:55 -0300

    Spring Boot - Init

---
+++ src/main/java/com/pismo/transactionroutines/TransactionroutinesApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.transactionroutines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionroutinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionroutinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pismo/transactionroutines/TransactionroutinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.transactionroutines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionroutinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
