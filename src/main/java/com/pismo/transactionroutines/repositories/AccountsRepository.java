3e4bf95ecd6280358ce53429e295f5c528156730
William Pereira <williamfunchal@outlook.com>
2020-06-01 16:06:01 -0300

    AccountsRepository added with GetByAccountID method

---
+++ src/main/java/com/pismo/transactionroutines/repositories/AccountsRepository.java
@@ -0,0 +1,16 @@
+package com.pismo.transactionroutines.repositories;
+
+import com.pismo.transactionroutines.domain.Account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Account, Long> {
+
+    @Query(value = "SELECT Account_ID, Document_Number FROM Accounts a WHERE a.Account_ID = :accountIDParam", nativeQuery = true)
+    Account GetByAccountID(@Param("accountIDParam") Long accountID);
+
+}
