bbbb4f5a23f7ff8181d4f0025ea1d3cb3329d800
William Pereira <williamfunchal@outlook.com>
2020-06-02 17:20:31 -0300

    getByAccountId - name convention fixed

--- src/main/java/com/pismo/transactionroutines/repositories/AccountsRepository.java
+++ src/main/java/com/pismo/transactionroutines/repositories/AccountsRepository.java
@@ -4,11 +4,12 @@ import com.pismo.transactionroutines.domain.Account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountsRepository extends JpaRepository<Account, Long> {
+public interface AccountsRepository extends CrudRepository<Account, Long> {
 
     @Query(value = "SELECT account_id, document_number FROM accounts a WHERE a.account_id = :accountIdParam", nativeQuery = true)
     Account getByAccountId(@Param("accountIdParam") Long accountId);
--- src/main/java/com/pismo/transactionroutines/repositories/TransactionsRepository.java
+++ src/main/java/com/pismo/transactionroutines/repositories/TransactionsRepository.java
@@ -6,11 +6,12 @@ import java.util.List;
 import com.pismo.transactionroutines.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionsRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionsRepository extends CrudRepository<Transaction, Long> {
 
     
 
