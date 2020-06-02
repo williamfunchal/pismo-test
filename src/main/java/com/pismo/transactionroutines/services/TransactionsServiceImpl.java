77a6d3ed9a76fea96e35c9e8811bbb00cb2da2d9
William Pereira <williamfunchal@outlook.com>
2020-06-02 18:47:09 -0300

    Transaction Service improvement - accountsRepository injection - account verification

--- src/main/java/com/pismo/transactionroutines/services/TransactionsServiceImpl.java
+++ src/main/java/com/pismo/transactionroutines/services/TransactionsServiceImpl.java
@@ -3,10 +3,13 @@ package com.pismo.transactionroutines.services;
 import java.util.Date;
 
 import com.pismo.transactionroutines.domain.Transaction;
+import com.pismo.transactionroutines.repositories.AccountsRepository;
 import com.pismo.transactionroutines.repositories.TransactionsRepository;
 import com.pismo.transactionroutines.services.interfaces.AccountService;
 
 import com.pismo.transactionroutines.services.interfaces.TransactionService;
+import com.pismo.transactionroutines.util.exceptions.AccountNotFoundException;
+import com.pismo.transactionroutines.util.exceptions.AccountNotInformedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +17,23 @@ import org.springframework.stereotype.Service;
 public class TransactionsServiceImpl implements TransactionService {
 
     private TransactionsRepository transactionsRepository;
+    private AccountsRepository accountsRepository;
 
     @Autowired
-    public TransactionsServiceImpl(TransactionsRepository transactionsRepository) {
+    public TransactionsServiceImpl(TransactionsRepository transactionsRepository, AccountsRepository accountsRepository) {
         this.transactionsRepository = transactionsRepository;
+        this.accountsRepository = accountsRepository;
     }
 
     public Transaction insert(Transaction transaction) {
         if (transaction == null)
             throw new RuntimeException();
+
+        if(transaction.getAccountId() == null)
+            throw new AccountNotInformedException("Account not Informed.");
+
+        if(accountsRepository.getByAccountId(transaction.getAccountId()) == null)
+            throw new AccountNotFoundException("Account not found.");
 
         transaction.setEventDate(new Date());
 
